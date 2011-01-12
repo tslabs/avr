3e893b8121e1379b6fb7807a76b6f6b54d60ac94
at2313 <at2313@localhost>
2011-01-12 19:22:04 +0000

    Some neats

--- AVR_MP_16/AvrBuild.bat
+++ AVR_MP_16/AvrBuild.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "c:\@\AVR_MP_16\labels.tmp" -fI -W+ie -C V2E -o "c:\@\AVR_MP_16\AVR_MP_16.hex" -d "c:\@\AVR_MP_16\AVR_MP_16.obj" -e "c:\@\AVR_MP_16\AVR_MP_16.eep" -m "c:\@\AVR_MP_16\AVR_MP_16.map" "c:\@\AVR_MP_16\AVR_MP_16.asm"
+"C:\Program Files\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "d:\_Heap\Work\SVN\tsl-avr\AVR_MP_16\labels.tmp" -fI -W+ie -C V2E -o "d:\_Heap\Work\SVN\tsl-avr\AVR_MP_16\AVR_MP_16.hex" -d "d:\_Heap\Work\SVN\tsl-avr\AVR_MP_16\AVR_MP_16.obj" -e "d:\_Heap\Work\SVN\tsl-avr\AVR_MP_16\AVR_MP_16.eep" -m "d:\_Heap\Work\SVN\tsl-avr\AVR_MP_16\AVR_MP_16.map" "d:\_Heap\Work\SVN\tsl-avr\AVR_MP_16\AVR_MP_16.asm"
