f7f18c291897a930290945fc886e14652d57fca5
at2313 <at2313@localhost>
2011-10-16 21:02:50 +0000

    AVR RS232: garbage collected

--- AVR RS232/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "c:\@\RS232\labels.tmp" -fI -W+ie -C V2E -o "c:\@\RS232\RS232.hex" -d "c:\@\RS232\RS232.obj" -e "c:\@\RS232\RS232.eep" -m "c:\@\RS232\RS232.map" "c:\@\RS232\RS232.asm"
